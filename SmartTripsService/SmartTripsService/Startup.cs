8ea487a812c4ab66eed1577e3db4ce79a093fe11
Fernando Tubio <fernando.tubio@southworks.net>
2016-03-16 13:53:44 -0700

    Initial commit

---
+++ SmartTripsService/SmartTripsService/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SmartTripsService.Startup))]
+
+namespace SmartTripsService
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
